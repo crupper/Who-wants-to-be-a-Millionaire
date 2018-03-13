651dc625b03d9f0d98c30550acb869b5ec082131
Chris Rupper <chrisrupper@gmail.com>
2018-03-13 16:46:07 -0600

    setup basic server/routes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,44 @@
 'use strict';
 
-const express = require('express');
+// Initialize tools ===========================================================
+const express       = require('express')
+const app           = express()
+const port          = process.env.PORT || 3000
+const path          = require('path')
+const passport      = require('passport')
+const flash         = require('connect-flash')
 
+const morgan        = require('morgan')
+const cookieParser  = require('cookie-parser')
+const bodyParser    = require('body-parser')
+const session       = require('express-session')
+// var configDB = require('./config/database.js');
 
-const app = express()
+// configure ==================================================================
+// require('./authentication/passport')(passport); // pass passport for configuration
 
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// app.set('views', __dirname + '/../www/')
+app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('views', path.join(__dirname, '/../www/'));
+
+// required for passport
+app.use(session({ secret: 'whowantstobeamillionaire' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes =====================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // Server listen on port
-app.listen(3000, err  => {
+app.listen(port, err  => {
     if(err) console.error(err.stack)
-    console.log('App listening on port 3000!')
+    console.log('App listening on port ' + port)
   })
 // Serve static directory
 app.use(express.static('www'))

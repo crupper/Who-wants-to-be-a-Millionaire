f92a24ddad7f933179a9911a166bcafc3013d9f2
Chris Rupper <chrisrupper@gmail.com>
2018-04-16 12:05:46 -0600

    fixed end game issues for single user

--- www/js/game.js
+++ www/js/game.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         updateUserStanding()
         loadCurrentQuestion()
     }
+    updateUserStanding()
 }, false);
 
 loadQuestion = function() {
